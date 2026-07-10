f4617fd4c01e8782336cde963d9fd2fc1994cf15
Piyush <pk3698855@gmail.com>
2026-07-10 16:15:52 +0530

    add extraction loigc

---
+++ src/lib/extract.ts
@@ -0,0 +1,31 @@
+import { raw } from "@prisma/client/runtime/client";
+import mammoth from "mammoth";
+
+export async function ExtractText(buffer: Buffer, mimetype: string): Promise<string> {
+    let rawText: string;
+
+    switch (mimetype) {
+        case "application/pdf": {
+            const pdfParseModule = await (import("pdf-parse")) as any;
+            const pdfParse = pdfParseModule.default || pdfParseModule;
+            const data = await pdfParse(buffer);
+            rawText = data.text;
+            break;
+        }
+        case "application/vnd.openxmlformats-officedocument.wordprocessingml.document": {
+            const { value } = await mammoth.extractRawText({ buffer });
+            rawText = value;
+            break;
+        }
+        case "text/plain":
+        case "text/markdown": {
+            rawText = buffer.toString("utf-8");
+            break;
+        }
+        default: throw Error(`Unsupported mimetpye for extraction: ${mimetype}`);
+    }
+    if (!rawText.trim()) {
+        throw new Error("Extraction produced no text ( possibly  a scanned/image-only file ) ")
+    }
+    return rawText.replace(/\u0000/g, "");
+}
